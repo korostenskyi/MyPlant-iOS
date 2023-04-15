dd8bf606746f3fdb156374645f98ffd77aed42be
Roman Korostenskyi <romankorostensky@gmail.com>
2023-04-15 19:57:12 +0300

    Initial Commit

---
+++ MyPlant/MyPlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlantApp.swift
+//  MyPlant
+//
+//  Created by Roman Korostenskyi on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
